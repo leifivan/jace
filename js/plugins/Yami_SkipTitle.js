69e9549392c4db7230ab4d4ebd537b6e8c15b402
Leif Ivan <leafvans@gmail.com>
2026-09-03 00:27:36 +0900

    Update

--- js/plugins/Yami_SkipTitle.js
+++ js/plugins/Yami_SkipTitle.js
@@ -1,22 +1,47 @@
 /*:
  * @plugindesc Skip the title scene for testing purpose.
- * @version 1.0
+ * @author Yami
+ * @version 1.1
+ *
+ * @help
+ * Skips the title screen and starts a new game.
+ *
+ * Compatible with MOG - Title Splash Screen: splash logos still play,
+ * then the game starts instead of showing the title menu.
+ *
+ * Returning to the title later (game over, game end) is not skipped.
  */
 
 (function() {
 
-    Scene_Boot.prototype.start = function() {
-        Scene_Base.prototype.start.call(this);
-        SoundManager.preloadImportantSounds();
-        if (DataManager.isBattleTest()) {
-            DataManager.setupBattleTest();
-            SceneManager.goto(Scene_Battle);
-        } else {
-            this.checkPlayerLocation();
+    var _SceneManager_goto = SceneManager.goto;
+    SceneManager.goto = function(sceneClass) {
+        if (sceneClass === Scene_Title && shouldSkipTitle()) {
             DataManager.setupNewGame();
-            SceneManager.goto(Scene_Map);
+            sceneClass = Scene_Map;
         }
-        this.updateDocumentTitle();
+        _SceneManager_goto.call(this, sceneClass);
     };
+
+    function shouldSkipTitle() {
+        var scene = SceneManager._scene;
+        if (!scene) {
+            return true;
+        }
+        if (scene instanceof Scene_Boot) {
+            return true;
+        }
+        // MOG - Title Splash Screen
+        if (typeof Scene_Splash_Screen !== 'undefined' &&
+                scene instanceof Scene_Splash_Screen) {
+            return true;
+        }
+        // MadeWithMv splash
+        if (typeof Scene_Splash !== 'undefined' &&
+                scene instanceof Scene_Splash) {
+            return true;
+        }
+        return false;
+    }
 
 })();
